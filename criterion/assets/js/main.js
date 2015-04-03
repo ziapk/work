30f2151484f24bb73e14361d123a75fe0824b305
Zia ur Rehman <zia.pccr@yahoo.com>
2015-04-04 00:40:43 +0500

    Update Criterion (New Project)

---
+++ criterion/assets/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+	var top = $('.navigation-area').offset().top;
+	var height = $('.navigation-area').height();
+	$(window).scroll(function (argument) {
+		if ($(this).scrollTop() > top) {
+          $('body').addClass('nav-fixed');
+          $('body').css('padding-top', height);
+	      } else {
+	          $('body').removeClass('nav-fixed');
+	          $('body').css('padding-top', 0);
+	      }
+	});
+	$('.scrollTo').on('click',function (e) {
+		    e.preventDefault();
+		    var target = this.hash;
+		    var $target = $(target);
+
+		    $('html, body').stop().animate({
+		        'scrollTop': $target.offset().top
+		    });
+	})
+	$('.navbar-toggle').on('click', function () {
+		$('.main-nav').slideToggle('fast');
+	})
+})
